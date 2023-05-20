ad081f38c9b30ced1ed5f22d45d58eee6ff70040
KovalYevhenii <kovalyevhenii@gmail.com>
2023-05-20 11:11:28 +0200

    V0.1

---
+++ MyTelegramBot/BotLogic/UpdateHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace MyTelegramBot.BotLogic
+{
+    public class UpdateHandler
+    {
+        private readonly ITelegramBotClient _botClient;
+        public UpdateHandler(ITelegramBotClient botClient)
+        {
+            _botClient = botClient;
+        }
+
+       public async Task HandleUpdateAsync(Update update, CancellationToken cancellationToken)
+        {
+
+            if (update.Message is not { } message)
+                return;
+            // Only process text messages
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            if (message.Text == "/start")
+            {
+                var keyboard = KeyBordBuilder.CreateKeyboard();
+
+                var replyMarkup = new ReplyKeyboardMarkup(button:"dwd");
+                Message sentMessage = await _botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: "Welkome to my bot. Select a command froo the keyboard:",
+                    replyMarkup: replyMarkup,
+                    cancellationToken: cancellationToken);  
+            }
+
+        }
+    }
+}
---
+++ MyTelegramBot/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using MyTelegramBot.BotLogic;
+using System.Reflection.Metadata;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+
+var botClient = new TelegramBotClient("5617391285:AAH28H0P9UYLh6LgKtz12lz4IxIrn3hDmjk");
+using CancellationTokenSource cts = new();
+
+ReceiverOptions receiverOptions = new()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+var updateHandler = new UpdateHandler(botClient);
+
+botClient.StartReceiving(
+  updateHandler: update => updateHandler.HandleUpdateAsync(update, cts.Token),
+   pollingErrorHandler: HandlePollingErrorAsync,
+   receiverOptions: receiverOptions
+ );
+
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.ReadLine();
+cts.Cancel();
+
+
+Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
+}
+
+
