e5d5a3bf0de38c4be30a5e7b64b36bb9ae6648a3
KovalYevhenii <kovalyevhenii@gmail.com>
2023-05-20 16:16:55 +0200

    first commit

--- MyTelegramBot/Program.cs
+++ MyTelegramBot/Program.cs
@@ -1,46 +1,22 @@
-﻿
-using MyTelegramBot.BotLogic;
-using System.Reflection.Metadata;
-using Telegram.Bot;
-using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
-
-
-var botClient = new TelegramBotClient("5617391285:AAH28H0P9UYLh6LgKtz12lz4IxIrn3hDmjk");
-using CancellationTokenSource cts = new();
-
-ReceiverOptions receiverOptions = new()
+﻿using Telegram.Bot;
+using System;
+using System.Threading.Tasks;
+namespace MyTelegramBot
 {
-    AllowedUpdates = Array.Empty<UpdateType>()
-};
-var updateHandler = new UpdateHandler(botClient);
-
-botClient.StartReceiving(
-  updateHandler: update => updateHandler.HandleUpdateAsync(update, cts.Token),
-   pollingErrorHandler: HandlePollingErrorAsync,
-   receiverOptions: receiverOptions
- );
-
-var me = await botClient.GetMeAsync();
-Console.WriteLine($"Start listening for @{me.Username}");
-Console.ReadLine();
-cts.Cancel();
-
-
-Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-{
-    var ErrorMessage = exception switch
+    class Programm
     {
-        ApiRequestException apiRequestException
-            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-        _ => exception.ToString()
-    };
+        static async Task Main(string[] args)
+        {
+            await StartBotAsync();
+        }
+        static async Task StartBotAsync()
+        {
+            var botClient = new TelegramBotClient("5617391285:AAH28H0P9UYLh6LgKtz12lz4IxIrn3hDmjk");
 
-    Console.WriteLine(ErrorMessage);
-    return Task.CompletedTask;
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Bot is sucssesfully started user ID {me.Id}" +
+                $"my name is {me.Username}");
+        }
+    }
 }
-
-
