a969736e6622fbda70dc50164586e922c1f3fcd5
KovalYevhenii <kovalyevhenii@gmail.com>
2023-08-05 16:35:08 +0200

    update methods

--- MyTelegramBot/DBase/IUserRepository.cs
+++ MyTelegramBot/DBase/IUserRepository.cs
@@ -9,7 +9,7 @@ namespace MyTelegramBot.DBase
 {
     interface IUserRepository
     {
-        public void AddResource(string input, Update update,IMessageSender message);
+        public Task AddResource(string input, Update update, IMessageSender message);
         public void GetInfo(int id);
         public void RemovePreviousState(int id);
         public void UpdateState(int id, int newValue);
--- MyTelegramBot/DBase/UserRepository.cs
+++ MyTelegramBot/DBase/UserRepository.cs
@@ -12,49 +12,49 @@ namespace MyTelegramBot.DBase
     {
         private readonly string _connectionString;
         private readonly ITelegramBotClient _botClient;
+
         public UserRepository(string connectionString, ITelegramBotClient botClient)
         {
-            this._botClient = botClient;
-            _connectionString = connectionString;
+            _botClient = botClient;
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
         }
 
-        public async void AddResource(string input, Update update, IMessageSender message)
+        public async Task AddResource(string input,  Update update, IMessageSender message)
         {
             var userId = update.Message?.From?.Id;
-
+            var amount = InputValidator(input);
+          
             try
-            {
-                if (input.StartsWith("SE") && update.Message != null)
+            { 
+                if (amount.HasValue && amount.Value > 0 && userId.HasValue)
                 {
-                    string numInput = new(input.Where(char.IsDigit).ToArray());
-
-                    if (int.TryParse(numInput, out int amount))
+                    using (var con = new NpgsqlConnection(_connectionString))
                     {
-                        using (var con = new NpgsqlConnection(_connectionString))
+                        string insertQuery = "INSERT INTO resources(electricity,user_id) values(@amount,@userId)";
+                        await con.OpenAsync();
+                        using (var cmd = new NpgsqlCommand(insertQuery, con))
                         {
-                            string insertQuery = $"INSERT INTO resources(electricity,user_id) values({amount},{userId})";
+                            cmd.Parameters.AddWithValue("@amount", amount.Value);
+                            cmd.Parameters.AddWithValue("@userID", userId);
 
-                            con.Execute(insertQuery);
-
-                            await message.SendTextMessageAsync(_botClient, update.Message.Chat.Id, "Stored! Have nice day:)");
+                            await cmd.ExecuteNonQueryAsync();
                         }
-                    }
-                    else
-                    {
-                        await message.SendTextMessageAsync(_botClient, update.Message.Chat.Id, "Wrong input Value:(");
+                       con.Close();
+
+                        await message.SendTextMessageAsync(_botClient,update.Message.Chat.Id, "Stored! Have nice day:)");
                     }
                 }
+                else
+                {
+                    await message.SendTextMessageAsync(_botClient, update.Message.Chat.Id, "Sorry, your input was incorrect");
+                }
+
             }
             catch (Exception ex)
             {
-
-                throw new Exception(ex.Message);
+                Console.WriteLine("An error occurred: " + ex.Message);
             }
-
-
-
         }
-
 
         public void GetInfo(int id)
         {
@@ -65,17 +65,29 @@ namespace MyTelegramBot.DBase
         {
             throw new NotImplementedException();
         }
+
         public void UpdateState(int id, int newValue)
         {
             throw new NotImplementedException();
         }
+
         public void BalanceCounter()
         {
             throw new NotImplementedException();
         }
-        public void InputValidator(string input)
-        {
 
+        private int? InputValidator(string input)
+        {
+            if (input.StartsWith("SE") || input.StartsWith("SG"))
+            {
+                input = new(input.Where(char.IsDigit).ToArray());
+
+                if (int.TryParse(input, out int amount))
+                {
+                    return amount;
+                }
+            }
+            return null;
         }
     }
 }
