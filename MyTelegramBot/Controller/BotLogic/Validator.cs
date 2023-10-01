e4a2337b271886e8d93b14d610876e91ab73178a
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-01 15:48:38 +0200

    created extra class for validator

--- MyTelegramBot/Controller/BotLogic/Validator.cs
+++ MyTelegramBot/Controller/BotLogic/Validator.cs
@@ -1,40 +1,35 @@
-﻿using MyTelegramBot.Models.MessageHandler;
-
-using Telegram.Bot;
+﻿using Telegram.Bot;
 using Telegram.Bot.Types;
+namespace MyTelegramBot.Controller.BotLogic;
 
-namespace MyTelegramBot.Controller.BotLogic
+public class Validator
 {
-    public class Validator
+    private ITelegramBotClient _botClient;
+    private Chat _chat;
+    public Validator(ITelegramBotClient botClient, Chat chat)
     {
-        private ITelegramBotClient _botClient;
-        Chat _chat;
-        public Validator(ITelegramBotClient botClient, Chat chat)
+        _botClient = botClient;
+        _chat = chat;
+    }
+    public static int? InputValidator(string input)
+    {
+        if (input.StartsWith("SE") || input.StartsWith("SG"))
         {
-            _botClient = botClient;
-            _chat = chat;
-        }
-        public static int? InputValidator(string input)
-        {
-            if (input.StartsWith("SE") || input.StartsWith("SG"))
+            input = new(input.Where(char.IsDigit).ToArray());
+
+            if (int.TryParse(input, out int amount))
             {
-                input = new(input.Where(char.IsDigit).ToArray());
-
-                if (int.TryParse(input, out int amount))
-                {
-                    return amount;
-                }
+                return amount;
             }
-
-            return null;
         }
-        public async Task HandleValidationFailureAsync()
-        {
-            await _botClient.SendTextMessageAsync(_chat.Id, "Sorry, your input was incorrect");
-        }
-        public async Task HandleValidationSuccessAsync()
-        {
-            await _botClient.SendTextMessageAsync(_chat.Id, "Stored! Have nice day:)");
-        }
+        return null;
+    }
+    public async Task HandleValidationFailureAsync()
+    {
+        await _botClient.SendTextMessageAsync(_chat.Id, "Sorry, your input was incorrect");
+    }
+    public async Task HandleValidationSuccessAsync()
+    {
+        await _botClient.SendTextMessageAsync(_chat.Id, "Stored! Have nice day:)");
     }
 }
