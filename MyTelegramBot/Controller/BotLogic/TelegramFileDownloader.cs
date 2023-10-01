6b5f04bfdc6fcdba4d6e5a41e547c2af11f87cec
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-01 15:51:55 +0200

    minor changes

--- MyTelegramBot/Controller/BotLogic/TelegramFileDownloader.cs
+++ MyTelegramBot/Controller/BotLogic/TelegramFileDownloader.cs
@@ -26,40 +26,41 @@ namespace MyTelegramBot.Controller.BotLogic
         public async Task DownloadDocument()
         {
             var document = _update?.Message?.Document;
-            if (document == null)
-            {
-                await SendErrorMessage("Document was null or not provided.");
-                return;
-            }
-
-            OnDownloadStarted();
-
             try
             {
-                var fileId = document.FileId;
-                var fileInfo = await _botClient.GetFileAsync(fileId);
-                var filePath = fileInfo.FilePath;
-                string fileName = $"{DateTime.Now:dd.MM.yy}_{document.FileName}";
-
-                string destinationFilePath = _filePathProvider.GetDestinationFilePath(fileName);
-
-                if (System.IO.File.Exists(destinationFilePath))
+                if (document != null)
                 {
-                    await SendInfoMessage("File already exists. Skipping download.");
-                }
-                else
-                {
-                    await using Stream fileStream = System.IO.File.Create(destinationFilePath);
+                      OnDownloadStarted();
 
-                    if (!string.IsNullOrEmpty(filePath))
+                    var fileId = document.FileId;
+                    var fileInfo = await _botClient.GetFileAsync(fileId);
+                    var filePath = fileInfo.FilePath;
+                    string fileName = $"{DateTime.Now:dd.MM.yy}_{document.FileName}";
+
+                    string destinationFilePath = _filePathProvider.GetDestinationFilePath(fileName);
+
+                    if (System.IO.File.Exists(destinationFilePath))
                     {
-                        await _botClient.DownloadFileAsync(filePath, fileStream);
-                        OnDownloadCompleted();
+                        await SendInfoMessage("File already exists. Skipping download.");
                     }
                     else
                     {
-                        await SendErrorMessage("Invalid File path");
-                    }
+                        await using Stream fileStream = System.IO.File.Create(destinationFilePath);
+
+                        if (!string.IsNullOrEmpty(filePath))
+                        {
+                            await _botClient.DownloadFileAsync(filePath, fileStream);
+                            OnDownloadCompleted();
+                        }
+                        else
+                        {
+                            await SendErrorMessage("Invalid File path");
+                        }
+                    } 
+                }
+                else
+                {
+                    await SendErrorMessage("Document was null or not provided.");
                 }
             }
             catch (Exception ex)
@@ -70,12 +71,12 @@ namespace MyTelegramBot.Controller.BotLogic
 
         private async Task SendErrorMessage(string errorMessage)
         {
-            await _messageSender.SendTextMessageAsync(_botClient, _update.Message.Chat.Id, errorMessage);
+            await _messageSender.SendTextMessageAsync(_botClient, _update.Message!.Chat.Id, errorMessage);
         }
 
         private async Task SendInfoMessage(string infoMessage)
         {
-            await _messageSender.SendTextMessageAsync(_botClient, _update.Message.Chat.Id, infoMessage);
+            await _messageSender.SendTextMessageAsync(_botClient, _update.Message!.Chat.Id, infoMessage);
         }
 
         protected virtual void OnDownloadStarted()
