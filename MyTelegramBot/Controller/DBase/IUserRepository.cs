953ef5bf5c94c46810fc22811f5ec3ec2147e3ae
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-01 15:49:21 +0200

    improved querys

--- MyTelegramBot/Controller/DBase/IUserRepository.cs
+++ MyTelegramBot/Controller/DBase/IUserRepository.cs
@@ -1,14 +1,14 @@
 ﻿using MyTelegramBot.Controller.BotLogic;
 
-namespace MyTelegramBot.Controller.DBase
+namespace MyTelegramBot.Controller.DBase;
+
+interface IUserRepository
 {
-    interface IUserRepository
-    {
-        public Task AddResource(string input, Validator validator);
-        public Task UpdateBalanceElec();
-        public Task UpdateBalanceGas();
-        public Task<int> GetPreviousBalanceElec();
-        public Task<int> GetPreviousBalanceGas();
-        public Task<bool> DeleteResoure(string resourceName);
-    }
+    public Task AddUser();
+    public Task AddResource(string input, Validator validator);
+    public Task UpdateBalanceElec();
+    public Task UpdateBalanceGas();
+    public Task<int> GetPreviousBalanceElec();
+    public Task<int> GetPreviousBalanceGas();
+    public Task<bool> DeleteResoure(string resourceName);
 }
--- MyTelegramBot/Controller/DBase/UserRepository.cs
+++ MyTelegramBot/Controller/DBase/UserRepository.cs
@@ -1,203 +1,223 @@
 ﻿using Dapper;
 using MyTelegramBot.Controller.BotLogic;
-using MyTelegramBot.Models.MessageHandler;
 using Npgsql;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+namespace MyTelegramBot.Controller.DBase;
 
-namespace MyTelegramBot.Controller.DBase
+internal class UserRepository : IUserRepository
 {
-
-    internal class UserRepository : IUserRepository
+    private readonly string _connectionString;
+    private readonly ITelegramBotClient _botClient;
+    private readonly long _userId;
+    private readonly long _chatId;
+    public UserRepository(string connectionString, ITelegramBotClient botClient, Update update)
     {
-        private readonly string _connectionString;
-        private readonly ITelegramBotClient _botClient;
-        private readonly long _userId;
-        private readonly long _chatId;
-        public UserRepository(string connectionString, ITelegramBotClient botClient, Update update)
+        _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        _botClient = botClient;
+        _userId = update?.Message?.From?.Id ?? 0;
+        _chatId = update?.Message?.Chat.Id ?? 0;
+    }
+
+    public async Task AddResource(string input, Validator validator)
+    {
+        var amount = Validator.InputValidator(input);
+
+        try
         {
-            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
-            _botClient = botClient;
-            _userId = update?.Message?.From?.Id ?? 0;
-            _chatId = update?.Message?.Chat.Id ?? 0;
-        }
-
-        public async Task AddResource(string input,Validator validator)
-        {  
-            var amount = Validator.InputValidator(input);
-
-            try
+            if (amount.HasValue && amount.Value > 0 && amount != null)
             {
-                if (amount.HasValue && amount.Value > 0 && amount != null)
+                var tableName = input.StartsWith("SE") ? "electricity" : input.StartsWith("SG") ? "gas" : null;
+
+                if (tableName != null)
                 {
-                    var tableName = input.StartsWith("SE") ? "electricity" : input.StartsWith("SG") ? "gas" : null;
-                    if (tableName != null)
+                    using (var con = new NpgsqlConnection(_connectionString))
                     {
-                        using (var con = new NpgsqlConnection(_connectionString))
+                        var query = $"INSERT INTO resources({tableName},user_id) VALUES(@amount,@userId)";
+                        await con.OpenAsync();
+
+                        using (var cmd = new NpgsqlCommand(query, con))
                         {
-                            var query = $"INSERT INTO resources({tableName},user_id) VALUES(@amount,@userId)";
-                            await con.OpenAsync();
+                            cmd.Parameters.AddWithValue("@amount", amount.Value);
+                            cmd.Parameters.AddWithValue("@userID", _userId);
 
-                            using (var cmd = new NpgsqlCommand(query, con))
-                            {
-                                cmd.Parameters.AddWithValue("@amount", amount.Value);
-                                cmd.Parameters.AddWithValue("@userID", _userId);
-
-                                await cmd.ExecuteNonQueryAsync();
-                                await validator.HandleValidationSuccessAsync();
-                            }
+                            await cmd.ExecuteNonQueryAsync();
+                            await validator.HandleValidationSuccessAsync();
                         }
                     }
                 }
-                else
-                {
-                    await validator.HandleValidationFailureAsync();
-                }
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                await validator.HandleValidationFailureAsync();
             }
         }
-
-        public async Task UpdateBalanceElec()
+        catch (NpgsqlException ex)
         {
-            try
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    public async Task UpdateBalanceElec()
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
             {
-                using (var con = new NpgsqlConnection(_connectionString))
-                {
-                    await con.OpenAsync();
-                    string selectQuery = $"SELECT distinct electricity FROM resources WHERE user_id = {_userId} ORDER BY electricity desc LIMIT 1 OFFSET 1";
+                await con.OpenAsync();
+                string selectQuery = $"SELECT distinct electricity FROM resources where electricity is not NULL Order By  electricity desc limit 1";
 
-                    var currentResourceValue = await con.QueryFirstOrDefaultAsync<int>(selectQuery);
-                    var previousBalanse = await GetPreviousBalanceElec();
-                    var difference = currentResourceValue - previousBalanse;
+                var currentResourceValue = await con.QueryFirstOrDefaultAsync<int>(selectQuery);
+                var previousBalanse = await GetPreviousBalanceElec();
+                if (currentResourceValue == 0)
+                    currentResourceValue = previousBalanse;
+                var difference = currentResourceValue - previousBalanse;
 
-                    string updateQuery = $"UPDATE users SET balance_electricity = {difference} WHERE user_id = {_userId}";
-                    await con.ExecuteAsync(updateQuery);
-                }
-                await BalanceOutput("balance_electricity");
+                string updateQuery = $"UPDATE users SET balance_electricity = {difference} WHERE user_id = {_userId}";
+                await con.ExecuteAsync(updateQuery);
             }
-            catch (Exception ex)
+            await BalanceOutput("balance_electricity");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    public async Task UpdateBalanceGas()
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                await con.OpenAsync();
+                string selectQuery = $"SELECT distinct gas FROM resources where gas is not NULL Order By  gas desc limit 1";
+                var currentResourceValue = await con.QueryFirstOrDefaultAsync<int?>(selectQuery);
+
+                var previousBalanse = await GetPreviousBalanceGas();
+                if (currentResourceValue == 0)
+                    currentResourceValue = previousBalanse;
+
+                var difference = currentResourceValue - previousBalanse;
+
+                string updateQuery = $"UPDATE users SET balance_gas = {difference} WHERE user_id = {_userId}";
+                await con.ExecuteAsync(updateQuery);
+            }
+            await BalanceOutput("balance_gas");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+
+    private async Task BalanceOutput(string resuorceType)
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
+            {
+                await con.OpenAsync();
+                string selectQuery = $"SELECT {resuorceType} FROM users WHERE user_id = {_userId}";
+
+                var balance = await con.QueryFirstOrDefaultAsync(selectQuery);
+
+                await _botClient.SendTextMessageAsync(_chatId, $"{string.Join(' ',balance)}");
             }
         }
-        private async Task BalanceOutput(string resuorceType)
+        catch (Exception ex)
         {
-            try
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+    public async Task<int> GetPreviousBalanceGas()
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
             {
-                using (var con = new NpgsqlConnection(_connectionString))
-                {
-                    await con.OpenAsync();
-                    string selectQuery = $"SELECT {resuorceType} FROM users WHERE user_id = {_userId}";
+                await con.OpenAsync();
 
-                    var balance = await con.QueryFirstOrDefaultAsync(selectQuery);
+                var selectQuery = $"SELECT distinct gas FROM resources WHERE gas is not null ORDER BY gas desc LIMIT 1 offset 1";
 
-                    await _botClient.SendTextMessageAsync(_chatId, $"{balance}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                var previousBalance = await con.QueryFirstOrDefaultAsync<int>(selectQuery);
+
+                return previousBalance;
             }
         }
-        public async Task UpdateBalanceGas()
+        catch (Exception ex)
         {
-            try
-            {
-                using (var con = new NpgsqlConnection(_connectionString))
-                {
-                    await con.OpenAsync();
-                    string selectQuery = $"SELECT distinct gas FROM resources WHERE user_id = {_userId} ORDER BY gas DESC LIMIT 1 offset 1";
-                    var currentResourceValue = await con.QueryFirstOrDefaultAsync<int?>(selectQuery);
+            Console.WriteLine("An error occurred: " + ex.Message);
 
-                    var previousBalanse = await GetPreviousBalanceGas();
-                    var difference = currentResourceValue - previousBalanse;
+            return 0;
+        }
+    }
 
-                    string updateQuery = $"UPDATE users SET balance_gas = {difference} WHERE user_id = {_userId}";
-                    await con.ExecuteAsync(updateQuery);
-                }
-                await BalanceOutput("balance_gas");
-            }
-            catch (Exception ex)
+    public async Task<int> GetPreviousBalanceElec()
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                var SelectQuery = $"SELECT distinct electricity FROM resources WHERE electricity is not null ORDER BY electricity desc LIMIT 1 offset 1";
+
+                var previousBalance = await con.QueryFirstOrDefaultAsync<int>(SelectQuery);
+
+                return previousBalance;
             }
         }
-        public async Task<int> GetPreviousBalanceGas()
+        catch (Exception ex)
         {
-            try
-            {
-                using (var con = new NpgsqlConnection(_connectionString))
-                {
-                    await con.OpenAsync();
+            Console.WriteLine("An error occurred: " + ex.Message);
 
-                    var selectQuery = $"SELECT distinct gas FROM resources WHERE user_id = {_userId} ORDER BY gas LIMIT 1";
-
-                    var previousBalance = await con.QueryFirstOrDefaultAsync<int>(selectQuery);
-
-                    return previousBalance;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-
-                return 0;
-            }
+            return 0;
         }
+    }
 
-        public async Task<int> GetPreviousBalanceElec()
+    public async Task<bool> DeleteResoure(string resourceName)
+    {
+        try
         {
-            try
+            using (var con = new NpgsqlConnection(_connectionString))
             {
-                using (var con = new NpgsqlConnection(_connectionString))
+                var selectQuery = $"SELECT{resourceName},MAX(timestamp)from resources GROUP BY{resourceName}";
+                DateTime? lastTimeStamp = await con.ExecuteScalarAsync<DateTime>(selectQuery);
+
+                if (lastTimeStamp.HasValue)
                 {
-                    var SelectQuery = $"SELECT distinct electricity FROM resources WHERE user_id = {_userId} ORDER BY electricity  LIMIT 1";
+                    string deleteQuery = "DELETE from resources where timestamp = @lastTimeStamp";
+                    var rowsAffected = await con.ExecuteAsync(deleteQuery, new { lastTimeStamp });
 
-                    var previousBalance = await con.QueryFirstOrDefaultAsync<int>(SelectQuery);
-
-                    return previousBalance;
+                    return rowsAffected > 0;
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-
-                return 0;
-            }
-        }
-
-        public async Task<bool> Deleteresoure(string resourceName)
-        {
-            try
-            {
-                using (var con = new NpgsqlConnection(_connectionString))
-                {
-                    var selectQuery = $"SELECT{resourceName},MAX(timestamp)from resources GROUP BY{resourceName}";
-                    DateTime? lastTimeStamp = await con.ExecuteScalarAsync<DateTime>(selectQuery);
-
-                    if (lastTimeStamp.HasValue)
-                    {
-                        string deleteQuery = $"DELETE from resources where timestamp = @lastTimeStamp";
-                        var rowsAffected = await con.ExecuteAsync(deleteQuery, new { lastTimeStamp });
-
-                        return rowsAffected > 0;
-                    }
-                    return false;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
                 return false;
             }
         }
-
-        public Task<bool> DeleteResoure(string resourceName)
+        catch (Exception ex)
         {
-            throw new NotImplementedException();
+            Console.WriteLine("An error occurred: " + ex.Message);
+            return false;
+        }
+    }
+
+    public async Task AddUser()
+    {
+        try
+        {
+            using (var con = new NpgsqlConnection(_connectionString))
+            {
+                var SelectQuery = $"select add_user({_userId})";
+                await con.OpenAsync();
+                await con.ExecuteAsync(SelectQuery);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("User validation procedure" + ex.Message);
         }
     }
 }
