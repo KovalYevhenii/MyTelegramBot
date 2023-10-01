39d525910f24ce0e1b4fd240d20051c8868d0cab
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-01 15:50:38 +0200

    minor changes

--- MyTelegramBot/Controller/BotLogic/ChooseMenuController.cs
+++ MyTelegramBot/Controller/BotLogic/ChooseMenuController.cs
@@ -1,75 +1,74 @@
 ﻿using MyTelegramBot.View;
 using Telegram.Bot.Types;
-namespace MyTelegramBot.Controller.BotLogic
+namespace MyTelegramBot.Controller.BotLogic;
+
+internal class ChooseMenuController
 {
-    internal class ChooseMenuController
+
+    private readonly ChooseMenu _chooseMenu;
+    public ChooseMenuController(ChooseMenu chooseMenu)
     {
-    
-        private readonly ChooseMenu _chooseMenu;
-        public ChooseMenuController(ChooseMenu chooseMenu)
-        {
-            _chooseMenu = chooseMenu;
-        }
-        public async Task HandleCallbackQueryAsync(Update update, CallbackQuery callbackQuery)
-        {
-            _chooseMenu.SetMenuState(ChooseMenu.MenuState.Settings);
-            switch(callbackQuery.Data)
-            {
-                case "elec":
-                    {
-                        await _chooseMenu.HandleElecOptionAsync();
-                        break;
-                    }
-                case "gas":
-                    {  
-                        await _chooseMenu.HandleGasOptionAsync();
-                        break;
-                    }
-                case "downloadElec":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.DownloadE);
-                        await _chooseMenu.HandleDownloadElecAsync();
-                        break;
-                    }
-
-                case "downloadGas":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.DownloadG);
-                        await _chooseMenu.HandleDownloadGasAsync();
-                        break;
-                    }
-                case "stateElec":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.StateE);
-                        await _chooseMenu.HandleStateElecAsync(update);
-                        break;
-                    }
-
-                case "stateGas":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.StateG);
-                        await _chooseMenu.HandleStateGasAsync(update);
-                        break;
-                    }
-                case "balanceE":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.BalanceE);
-                        await _chooseMenu.HandleBalanceAsync();
-                        break;
-                    }
-                case "balanceG":
-                    {
-                        _chooseMenu.SetMenuState(ChooseMenu.MenuState.BalanceG);
-                        await _chooseMenu.HandleBalanceAsync();
-                        break;
-                    }
-                case "return":
-                    {
-                        await _chooseMenu.HandleReturnAsync();
-                        break;
-                    }
-            }
-        }
-
+        _chooseMenu = chooseMenu;
     }
+    public async Task HandleCallbackQueryAsync(Update update, CallbackQuery callbackQuery)
+    {
+        _chooseMenu.SetMenuState(ChooseMenu.MenuState.Settings);
+        switch(callbackQuery.Data)
+        {
+            case "elec":
+                {
+                    await _chooseMenu.HandleElecOptionAsync();
+                    break;
+                }
+            case "gas":
+                {  
+                    await _chooseMenu.HandleGasOptionAsync();
+                    break;
+                }
+            case "downloadElec":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.DownloadE);
+                    await _chooseMenu.HandleDownloadElecAsync();
+                    break;
+                }
+
+            case "downloadGas":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.DownloadG);
+                    await _chooseMenu.HandleDownloadGasAsync();
+                    break;
+                }
+            case "stateElec":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.StateE);
+                    await _chooseMenu.HandleStateElecAsync(update);
+                    break;
+                }
+
+            case "stateGas":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.StateG);
+                    await _chooseMenu.HandleStateGasAsync(update);
+                    break;
+                }
+            case "balanceE":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.BalanceE);
+                    await _chooseMenu.HandleBalanceAsync();
+                    break;
+                }
+            case "balanceG":
+                {
+                    _chooseMenu.SetMenuState(ChooseMenu.MenuState.BalanceG);
+                    await _chooseMenu.HandleBalanceAsync();
+                    break;
+                }
+            case "return":
+                {
+                    await _chooseMenu.HandleReturnAsync();
+                    break;
+                }
+        }
+    }
+
 }
--- MyTelegramBot/View/ChooseMenu.cs
+++ MyTelegramBot/View/ChooseMenu.cs
@@ -2,138 +2,138 @@
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
-namespace MyTelegramBot.View
+namespace MyTelegramBot.View;
+
+public class ChooseMenu
 {
-    public class ChooseMenu
+    private readonly ITelegramBotClient _botClient;
+    private readonly Chat _chat;
+    private MenuState _menuState;
+
+    public ChooseMenu(ITelegramBotClient botClient, Chat chat)
     {
-        private readonly ITelegramBotClient _botClient;
-        private readonly Chat _chat;
-        private MenuState _menuState;
+        _botClient = botClient;
+        _chat = chat;
+    }
+    public enum MenuState
+    {
+        Main,
+        Settings,
+        DownloadE,
+        DownloadG,
+        StateE,
+        StateG,
+        BalanceE,
+        BalanceG
+    }
+    public MenuState GetMenuState()
+    {
+        return _menuState;
+    }
+    public void SetMenuState(MenuState newState)
+    {
+        _menuState = newState;
+    }
 
-        public ChooseMenu(ITelegramBotClient botClient, Chat chat)
-        {
-            _botClient = botClient;
-            _chat = chat;
-        }
-        public enum MenuState
-        {
-            Main,
-            Settings,
-            DownloadE,
-            DownloadG,
-            StateE,
-            StateG,
-            BalanceE,
-            BalanceG
-        }
-        public MenuState GetMenuState()
-        {
-            return _menuState;
-        }
-        public void SetMenuState(MenuState newState)
-        {
-            _menuState = newState;
-        }
+    public async Task StartMenu()
+    {
+        InlineKeyboardMarkup inlineKeyboard = new(
+              new[] {
+                new[]
+                {
+                    InlineKeyboardButton.WithUrl("check Tarif", "check.24")
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("handle electricity resources", "elec")
+                },
+                new[]
+                {
+                    InlineKeyboardButton.WithCallbackData("Handle Gas resuorces", "gas")
+                }
+              });
 
-        public async Task StartMenu()
-        { 
-            InlineKeyboardMarkup inlineKeyboard = new(
-                  new[] {
-                    new[]
-                    {
-                        InlineKeyboardButton.WithUrl("check Tarif", "check.24")
-                    },
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("handle electricity resources", "elec")
-                    },
-                    new[]
-                    {
-                        InlineKeyboardButton.WithCallbackData("Handle Gas resuorces", "gas")
-                    }
-                  });
+        await _botClient.SendTextMessageAsync(_chat.Id,
+                $"Choose what you want to do",
+                replyMarkup: inlineKeyboard);
+    }
+    public async Task HandleElecOptionAsync()
+    {
+        InlineKeyboardMarkup inlineKeyboard = new(
+                   new[] {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData("Download electricity meter screenshot","downloadElec"),
 
-            await _botClient.SendTextMessageAsync(_chat.Id,
-                    $"Choose what you want to do",
-                    replyMarkup: inlineKeyboard);
-        }
-        public async Task HandleElecOptionAsync()
+                            InlineKeyboardButton.WithCallbackData("Enter meter state","stateElec"),
+
+                            InlineKeyboardButton.WithCallbackData("my Balance", "balanceE")
+
+                        },
+                         new[]
+                         {
+                            InlineKeyboardButton.WithCallbackData("⏪","return")
+
+                         }
+                   });
+        await _botClient.SendTextMessageAsync(_chat, "Choose option", replyMarkup: inlineKeyboard);
+    }
+    public async Task HandleGasOptionAsync()
+    {
+        InlineKeyboardMarkup inlineKeyboard = new(
+                   new[] {
+                        new[]
+                        {
+                            InlineKeyboardButton.WithCallbackData("Download Gas meter screenshot","downloadGas"),
+
+                            InlineKeyboardButton.WithCallbackData("Enter meter state","stateGas"),
+
+                              InlineKeyboardButton.WithCallbackData("my Balance", "balanceG")
+                        },
+                         new[]
+                         {
+                            InlineKeyboardButton.WithCallbackData("⏪","return")
+                         }
+                   });
+
+        await _botClient.SendTextMessageAsync(_chat, "Choose option", replyMarkup: inlineKeyboard);
+    }
+    public async Task HandleDownloadElecAsync()
+    {
+        SetMenuState(MenuState.DownloadE);
+        await _botClient.SendTextMessageAsync(_chat, "Please send me your screenshot as Dokument for downloading.");
+    }
+    public async Task HandleDownloadGasAsync()
+    {
+        SetMenuState(MenuState.DownloadG);
+        await _botClient.SendTextMessageAsync(_chat, "Please send me your screenshot as Dokument for downloading.");
+    }
+
+    public async Task HandleStateElecAsync(Update update)
+    {
+        SetMenuState(MenuState.StateE);
+        if (update.CallbackQuery != null)
         {
-            InlineKeyboardMarkup inlineKeyboard = new(
-                       new[] {
-                            new[]
-                            {
-                                InlineKeyboardButton.WithCallbackData("Download electricity meter screenshot","downloadElec"),
-
-                                InlineKeyboardButton.WithCallbackData("Enter meter state","stateElec"),
-
-                                InlineKeyboardButton.WithCallbackData("Get my State", "balanceE")
-
-                            },
-                             new[]
-                             {
-                                InlineKeyboardButton.WithCallbackData("⏪","return")
-
-                             }
-                       });
-            await _botClient.SendTextMessageAsync(_chat, "Choose option", replyMarkup: inlineKeyboard);
+            await _botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, " Instruction:\n Enter keyword- SE then provide a state", showAlert: true);
         }
-        public async Task HandleGasOptionAsync()
-        {
-            InlineKeyboardMarkup inlineKeyboard = new(
-                       new[] {
-                            new[]
-                            {
-                                InlineKeyboardButton.WithCallbackData("Download Gas meter screenshot","downloadGas"),
+    }
 
-                                InlineKeyboardButton.WithCallbackData("Enter meter state","stateGas"),
-
-                                  InlineKeyboardButton.WithCallbackData("Get my State", "balanceG")
-                            },
-                             new[]
-                             {
-                                InlineKeyboardButton.WithCallbackData("⏪","return")
-                             }
-                       });
-
-            await _botClient.SendTextMessageAsync(_chat, "Choose option", replyMarkup: inlineKeyboard);
-        }
-        public async Task HandleDownloadElecAsync()
+    public async Task HandleStateGasAsync(Update update)
+    {
+        SetMenuState(MenuState.StateG);
+        if (update.CallbackQuery != null)
         {
-            SetMenuState(MenuState.DownloadE);
-            await _botClient.SendTextMessageAsync(_chat, "Please send me your screenshot as Dokument for downloading.");
+            await _botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, " Instruction:\n Enter keyword- SG then provide a state", showAlert: true);
         }
-        public async Task HandleDownloadGasAsync()
-        {
-            SetMenuState(MenuState.DownloadG);
-            await _botClient.SendTextMessageAsync(_chat, "Please send me your screenshot as Dokument for downloading.");
-        }
-
-        public async Task HandleStateElecAsync(Update update)
-        {
-            SetMenuState(MenuState.StateE);
-            if (update.CallbackQuery != null)
-            {
-                await _botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, " Instruction:\n Enter keyword- SE then provide a state", showAlert: true);
-            }
-        } 
-        public async Task HandleStateGasAsync(Update update)
-        {
-            SetMenuState(MenuState.StateG);
-            if (update.CallbackQuery != null)
-            {
-                await _botClient.AnswerCallbackQueryAsync(update.CallbackQuery.Id, " Instruction:\n Enter keyword- SG then provide a state", showAlert: true);
-            }
-        }
-        public async Task HandleBalanceAsync()
-        {
-            await _botClient.SendTextMessageAsync(_chat, "Here are your balance");
-        }
-
-        public async Task HandleReturnAsync()
-        {
-            SetMenuState(MenuState.Main);
-            await StartMenu();
-        }
+    }
+    public async Task HandleBalanceAsync()
+    {
+        await _botClient.SendTextMessageAsync(_chat, "Here are your balance");
+    }
+ 
+    public async Task HandleReturnAsync()
+    {
+        SetMenuState(MenuState.Main);
+        await StartMenu();
     }
 }
