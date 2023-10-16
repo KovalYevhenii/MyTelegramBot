4f0162a34b55d32579ea2a8e037df35720ed1941
KovalYevhenii <kovalyevhenii@gmail.com>
2023-10-16 12:04:22 +0200

    update

---
+++ MyTelegramBot/Models/Entities/Resources.cs
@@ -0,0 +1,8 @@
+﻿namespace MyTelegramBot.Models.Entityes;
+
+internal class Resources
+{
+    public int gas { get; set; }
+    public int elctricity { get; set; }
+    public int user_id { get; set; }
+}
--- MyTelegramBot/Models/Entityes/Resources.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyTelegramBot.Models.Entityes
-{
-    internal class Resources
-    {
-        public int gas { get; set; }
-        public int elctricity { get; set; }
-        public int user_id { get; set; }
-    }
-}
